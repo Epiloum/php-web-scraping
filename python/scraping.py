0d3459c75132209174fdddb506231205c1841baf
Jaewon Kim <epiloum@gmail.com>
2021-12-01 21:55:15 +0900

    add url and css selector

--- python/scraping.py
+++ python/scraping.py
@@ -5,7 +5,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrapWebpage(url, selector):
+    res = []
     html = BeautifulSoup(requests.get(url).text, 'html.parser');
-    return html.select(selector)[0].text
+    for v in html.select(selector):
+        res.append(v.text)
+    return res
+
+
+url = 'https://dev.epiloum.net'
+selector = '#primary-menu li a';
 
 print(scrapWebpage(url, selector))
